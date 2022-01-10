58feb2897c8b82e0ba6cded83f63bf651684794a
osugimoto2 <osugimoto.25@acsamman.edu.jo>
2022-01-10 09:55:14 +0200

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -2,21 +2,23 @@ var width=getWidth();
 var height=getHeight();
 
 function start(){
-	
+	var first=readBoolean("Do you want be first? Yes, No");
 	var rect=newRectangle(width,10);
 	rect.setPosition(0,height/3);
 	rect.setColor(Color.black);
 	add(rect);
 	var rect2=newRectangle(width,10);
-	rect.setPosition(0,(height/3)*2;
-	rect.setColor(Color.black);
+	rect2.setPosition(0,(height/3)*2;
+	rect2.setColor(Color.black);
 	add(rect2);
 	var rect3=newRectangle(10,height);
-	rect.setPosition(width/3,0);
-	rect.setColor(Color.black);
+	rect3.setPosition(width/3,0);
+	rect3.setColor(Color.black);
 	add(rect3);
 	var rect4=newRectangle(10,height);
-	rect.setPosition((width/3)*2,0);
-	rect.setColor(Color.black);
+	rect4.setPosition((width/3)*2,0);
+	rect4.setColor(Color.black);
 	add(rect4);
+	var circle1=newCircle(10);
+	circle1.color(Color.black);
 }
